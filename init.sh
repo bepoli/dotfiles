b03caebeef2ca5b27a5f1ccf8743e3ed21549d45
Benedetto Polimeni <34317613+bepoli@users.noreply.github.com>
2023-07-24 15:53:57 +0200

    use sparse checkout

--- init.sh
+++ init.sh
@@ -7,6 +7,7 @@ export XDG_STATE_HOME="$HOME/.local/state"
 
 git clone --bare https://github.com/bepoli/dotfiles $XDG_CONFIG_HOME/dotfiles
 alias config='git --git-dir=$XDG_CONFIG_HOME/dotfiles --work-tree=$HOME'
+config sparse-checkout set --no-cone '/*' '!init.sh'
 config checkout
 if [ $? -gt 0 ]; then
   mkdir -p $XDG_CONFIG_HOME/dotfiles-backup
@@ -14,3 +15,5 @@ if [ $? -gt 0 ]; then
   config checkout
 fi
 config config --local status.showUntrackedFiles no
+
+mkdir -p $XDG_STATE_HOME/bash
