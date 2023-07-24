364c17f0b096f79240916c097bd6bdf48c61aa0f
Benedetto Polimeni <34317613+bepoli@users.noreply.github.com>
2023-07-24 17:59:17 +0200

    accept arguments to configure remote and outdir

--- init.sh
+++ init.sh
@@ -1,19 +1,36 @@
 #!/bin/sh
 
+# set up XDG environment as in dotfiles
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_STATE_HOME="$HOME/.local/state"
 
-git clone --bare https://github.com/bepoli/dotfiles $XDG_CONFIG_HOME/dotfiles
-alias config='git --git-dir=$XDG_CONFIG_HOME/dotfiles --work-tree=$HOME'
+# create bash state dir used to store HISTFILE
+mkdir -p "$XDG_STATE_HOME"/bash
+
+# parse arguments and set defaults
+REPODIR="$XDG_CONFIG_HOME"/dotfiles
+REMOTE='https://github.com/bepoli/dotfiles'
+while getopts 'd:r:' opt; do
+    case "$opt" in
+        'd')
+            REPODIR="$OPTARG"
+            ;;
+        'r')
+            REMOTE="$OPTARG"
+            ;;
+    esac
+done
+
+# initialize dotfiles
+git clone --bare "$REMOTE" "$REPODIR"
+alias config='git --git-dir="$REPODIR" --work-tree="$HOME"'
 config sparse-checkout set --no-cone '/*' '!init.sh' '!README.md'
 config checkout
 if [ $? -gt 0 ]; then
-  mkdir -p $XDG_CONFIG_HOME/dotfiles-backup
-  config checkout 2>&1 | grep -E "\s+\." | awk {'print $1'} | xargs -I{} mv {} $XDG_CONFIG_HOME/dotfiles-backup/{}
-  config checkout
+    mkdir -p ${REPODIR}-backup
+    config checkout 2>&1 | grep -E "\s+\." | awk {'print $1'} | xargs -I{} mv {} "$REPODIR"-backup/{}
+    config checkout
 fi
 config config --local status.showUntrackedFiles no
-
-mkdir -p $XDG_STATE_HOME/bash
