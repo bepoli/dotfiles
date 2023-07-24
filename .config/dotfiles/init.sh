4993699556a7b96c1c1bf24442892ce1ee9d57e5
Benedetto Polimeni <34317613+bepoli@users.noreply.github.com>
2023-07-24 14:59:45 +0200

    create state bash dir

--- .config/dotfiles/init.sh
+++ .config/dotfiles/init.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
-git clone --bare https://github.com/bepoli/dotfiles ~/.config/dotfiles
-alias config='git --git-dir=$HOME/.config/dotfiles --work-tree=$HOME'
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_STATE_HOME="$HOME/.local/state"
+
+git clone --bare https://github.com/bepoli/dotfiles $XDG_CONFIG_HOME/dotfiles
+alias config='git --git-dir=$XDG_CONFIG_HOME/dotfiles --work-tree=$HOME'
 config checkout
 if [ $? -gt 0 ]; then
-  mkdir -p ~/.config/dotfiles-backup
-  config checkout 2>&1 | grep -E "\s+\." | awk {'print $1'} | xargs -I{} mv {} ~/.config/dotfiles-backup/{}
+  mkdir -p $XDG_CONFIG_HOME/dotfiles-backup
+  config checkout 2>&1 | grep -E "\s+\." | awk {'print $1'} | xargs -I{} mv {} $XDG_CONFIG_HOME/dotfiles-backup/{}
   config checkout
 fi
 config config --local status.showUntrackedFiles no
+
+mkdir -p "$XDG_STATE_HOME"/bash
