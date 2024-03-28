8db1a67a40a052691b473e9b9fb19112d65a1096
Benedetto Polimeni <34317613+bepoli@users.noreply.github.com>
2024-03-28 11:48:02 +0100

    remove xdg and create .config if doesn't exists

--- init.sh
+++ init.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 
-# set up XDG environment as in dotfiles
-export XDG_CONFIG_HOME="$HOME/.config"
-export XDG_CACHE_HOME="$HOME/.cache"
-export XDG_DATA_HOME="$HOME/.local/share"
-export XDG_STATE_HOME="$HOME/.local/state"
-
-# create bash state dir used to store HISTFILE
-mkdir -p "$XDG_STATE_HOME"/bash
-
 # parse arguments and set defaults
-REPODIR="$XDG_CONFIG_HOME"/dotfiles
+if [ ! -d "$HOME/.config" ]; then
+    mkdir -p "$HOME/.config"
+fi
+REPODIR="$HOME/.config/dotfiles"
 REMOTE='https://github.com/bepoli/dotfiles'
 while getopts 'd:r:' opt; do
     case "$opt" in
