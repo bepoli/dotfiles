44bfd106e45cdf57ca4d228949c2c125b543a476
Benedetto Polimeni <polimeni@ingm.org>
2024-03-28 11:13:56 +0000

    remove wrong file

--- .config/bash/99-local.sh
+++
@@ -1,9 +0,0 @@
-
-P=/mnt/projects/labs/GEBI
-type "module" 2>/dev/null | grep -qwi function && module load slurm &>/dev/null
-
-export TOWER_ACCESS_TOKEN='eyJ0aWQiOiA4NDUxfS41YmVkNTc4OWFlNWU1NzQ4ZWZkNjhjNDgyOWQ4MzU3Mzg2NjA5Y2I1'
-export TOWER_WORKSPACE_ID=187637299803108
-
-alias fantomwget="wget -m -np -nH --cut-dirs=3 --user=dgtf6p2 --password='221007Fant*m6'"
-
