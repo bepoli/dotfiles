e592a5b8fdba1a96c4f9546dfca24baa9a589831
Benedetto Polimeni <polimeni@ingm.org>
2024-04-17 08:08:04 +0000

    make fzf-completions compatible with zsh

--- .config/shell/fzf-completions.sh
+++ .config/shell/fzf-completions.sh
@@ -6,11 +6,11 @@ if ! declare -F _fzf_complete &>/dev/null; then
 fi
 
 # Slurm process IDs
-_fzf_complete_slurm() {
+_fzf_complete_scancel() {
 	_fzf_complete --multi --header-lines=1 -- "$@" < <(squeue -u $USER)
 }
-_fzf_complete_slurm_post() {
+_fzf_complete_scancel_post() {
 	awk '{print $1}'
 }
-complete -F _fzf_complete_slurm -o default -o bashdefault scancel
+[ -n "$BASH" ] && complete -F _fzf_complete_scancel -o default -o bashdefault scancel || :
 
