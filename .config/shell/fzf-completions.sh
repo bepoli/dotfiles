0747a7a27b12bf3c3f52d766e4d7026b25fc5cc3
Benedetto Polimeni <34317613+bepoli@users.noreply.github.com>
2024-08-09 10:31:47 +0200

    remove fzf-z integration

--- .config/shell/fzf-completions.sh
+++ .config/shell/fzf-completions.sh
@@ -5,7 +5,7 @@ fi
 
 # Slurm process IDs
 _fzf_complete_scancel() {
-	_fzf_complete --multi --header-lines=1 -- "$@" < <(squeue -u $USER)
+	_fzf_complete --multi --header-lines=1 -- "$@" < <(squeue -u "$USER")
 }
 _fzf_complete_scancel_post() {
 	awk '{print $1}'
@@ -19,12 +19,3 @@ _fzf_complete_scontrol_post() {
 	awk '{print $1}'
 }
 [ -n "$BASH" ] && complete -F _fzf_complete_scontrol -o default -o bashdefault scancel || :
-
-# Z integration (https://github.com/junegunn/fzf/wiki/examples#z)
-if typeset -f _z &>/dev/null; then
-	unalias z 2> /dev/null
-	function z() {
-		[ $# -gt 0 ] && _z "$*" && return
-		cd "$(_z -l 2>&1 | fzf --height 40% --nth 2.. --reverse --inline-info +s --tac --query "${*##-* }" | sed 's/^[0-9,.]* *//')"
-	}
-fi
